fcf8c61c5fc59391987ab164bc42a4ba477fa9d7
Chen <chen.benyashar@mail.huji.ac.il>
2018-11-02 09:16:05 +0200

    add Question Class1

--- app/src/main/java/com/example/palabra/Question.java
+++ app/src/main/java/com/example/palabra/Question.java
@@ -6,13 +6,13 @@ public class Question
 {
     // ###  ATTRIBUTES   ###
     private String answer_a, answer_b, answer_c, answer_d;
-    private int points;
+    //    private int points;
     private int correct_ans;
-    String image;
+    ImageView image;
 
 
     // ###  CONSTRUCTOR   ###
-    public Question(String ans1, String ans2, String ans3, String ans4, int correct, String im)
+    public Question(String ans1, String ans2, String ans3, String ans4, int correct)
     {
         answer_a = ans1;
         answer_b = ans2;
@@ -20,45 +20,63 @@ public class Question
         answer_d = ans4;
 //        points = p;
         correct_ans = correct;
-        image = "4";
+//        image = im;
     }
 
 
     // ###  GETTER FUNCTIONS   ###
 
-    public int getCorrect_ans() { return correct_ans; }
+    int getCorrectAnsInt() { return correct_ans; }
 
-    public int getPoints() {  return points;  }
+    String getCorrectAnsStr() { return getAnswer(correct_ans); }
 
-    public String getAnswer_a()  {  return answer_a;  }
+//    public int getPoints() {  return points;  }
 
-    public String getAnswer_b()  {  return answer_b;  }
+//    public String getAnswer_a()  {  return answer_a;  }
+//
+//    public String getAnswer_b()  {  return answer_b;  }
+//
+//    public String getAnswer_c()  {  return answer_c;  }
+//
+//    public String getAnswer_d()  {  return answer_d;  }
 
-    public String getAnswer_c()  {  return answer_c;  }
-
-    public String getAnswer_d()  {  return answer_d;  }
-
-//    public ImageView getImage()  { return image;  }
-
-
-    // ###  CORRECTION FUNCTIONS   ###
-
-    public boolean is_a_correct()  {  return (correct_ans == 1);  }
-
-    public boolean is_b_correct()  {  return (correct_ans == 2);  }
-
-    public boolean is_c_correct()  {  return (correct_ans == 3);  }
-
-    public boolean is_d_correct()  {  return (correct_ans == 4);  }
-
-
-    public static Question get_question()
+    public String getAnswer(int choice)
     {
-        Question a = new Question("banana", "apple", "orange", "pair", 5, "6");
-        return a;
+        String answer = "fffff";
+        switch (choice){
+            case 1: answer =  answer_a;
+                break;
+            case 2: answer = answer_b;
+                break;
+            case 3: answer = answer_c;
+                break;
+            case 4: answer = answer_d;
+                break;
+        }
+        return answer;
     }
 
+//    public static void setImage(ImageView im)  { image = im;  }
 
+
+//    // ###  CORRECTION FUNCTIONS   ###
+//
+//    public boolean is_a_correct()  {  return (correct_ans == 1);  }
+//
+//    public boolean is_b_correct()  {  return (correct_ans == 2);  }
+//
+//    public boolean is_c_correct()  {  return (correct_ans == 3);  }
+//
+//    public boolean is_d_correct()  {  return (correct_ans == 4);  }
+
+
+    static Question get_question()
+    {
+
+
+        return new Question("banana", "apple", "orange", "pair", 1);
+
+    }
 
 
 }

93ec15706d21266dc74f1411625acaab2d2617e7
Chen <chen.benyashar@mail.huji.ac.il>
2018-11-02 09:22:44 +0200

    add Question Class removed comments

--- app/src/main/java/com/example/palabra/Question.java
+++ app/src/main/java/com/example/palabra/Question.java
@@ -6,10 +6,8 @@ public class Question
 {
     // ###  ATTRIBUTES   ###
     private String answer_a, answer_b, answer_c, answer_d;
-    //    private int points;
     private int correct_ans;
     ImageView image;
-
 
     // ###  CONSTRUCTOR   ###
     public Question(String ans1, String ans2, String ans3, String ans4, int correct)
@@ -18,27 +16,12 @@ public class Question
         answer_b = ans2;
         answer_c = ans3;
         answer_d = ans4;
-//        points = p;
         correct_ans = correct;
-//        image = im;
     }
 
-
     // ###  GETTER FUNCTIONS   ###
-
     int getCorrectAnsInt() { return correct_ans; }
-
     String getCorrectAnsStr() { return getAnswer(correct_ans); }
-
-//    public int getPoints() {  return points;  }
-
-//    public String getAnswer_a()  {  return answer_a;  }
-//
-//    public String getAnswer_b()  {  return answer_b;  }
-//
-//    public String getAnswer_c()  {  return answer_c;  }
-//
-//    public String getAnswer_d()  {  return answer_d;  }
 
     public String getAnswer(int choice)
     {
@@ -56,27 +39,8 @@ public class Question
         return answer;
     }
 
-//    public static void setImage(ImageView im)  { image = im;  }
-
-
-//    // ###  CORRECTION FUNCTIONS   ###
-//
-//    public boolean is_a_correct()  {  return (correct_ans == 1);  }
-//
-//    public boolean is_b_correct()  {  return (correct_ans == 2);  }
-//
-//    public boolean is_c_correct()  {  return (correct_ans == 3);  }
-//
-//    public boolean is_d_correct()  {  return (correct_ans == 4);  }
-
-
     static Question get_question()
     {
-
-
         return new Question("banana", "apple", "orange", "pair", 1);
-
     }
-
-
 }
